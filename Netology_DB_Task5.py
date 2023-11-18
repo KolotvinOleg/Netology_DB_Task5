e8f24b2bf5ca7b45e6ea7510c050d4f6a0ac80f7
Kolotvin_Oleg <Kolotvin.Oleg@gmail.com>
2023-11-18 10:53:17 +0400

    Correction

--- Netology_DB_Task5.py
+++ Netology_DB_Task5.py
@@ -3,23 +3,14 @@ import json
 from sqlalchemy.orm import sessionmaker
 from models import create_tables, Publisher, Book, Shop, Stock, Sale
 
-login = 'postgres'
-password = '*********'
-db_name = 'netology_db'
-DSN = f'postgresql://{login}:{password}@localhost:5432/{db_name}'
-engine = sq.create_engine(DSN)
 
-if __name__ == '__main__':
-    create_tables(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    with open('tests_data.json') as file:
+def insert_data(file_name):
+    with open(file_name) as file:
         info = json.load(file)
 
     for record in info:
         model = {
-            'publisher' : Publisher,
+            'publisher': Publisher,
             'book': Book,
             'shop': Shop,
             'stock': Stock,
@@ -29,15 +20,35 @@ if __name__ == '__main__':
         session.add(my_model(id=record.get('pk'), **record.get('fields')))
         session.commit()
 
-    name = input('Введите имя автора: ')
-    for c in session.query(
+
+def get_shops(author) -> None:
+    my_query = session.query(
         Book.title,
         Shop.name,
-        Sale.price*Sale.count,
-        Sale.date_sale)\
-        .join(Publisher, Publisher.id == Book.id_publisher).join(Stock, Book.id == Stock.id_book)\
-        .join(Shop, Stock.id_shop == Shop.id).join(Sale, Stock.id == Sale.id_stock)\
-        .filter(Publisher.name == name).all():
-        print(f'{c[0]:<40} | {c[1]:<8} | {c[2]:<6} | {c[3].strftime("%d-%m-%Y")}')
+        Sale.price * Sale.count,
+        Sale.date_sale). \
+        join(Publisher, Publisher.id == Book.id_publisher). \
+        join(Stock, Book.id == Stock.id_book). \
+        join(Shop, Stock.id_shop == Shop.id).join(Sale, Stock.id == Sale.id_stock)
+    if author.isdigit():
+        result = my_query.filter(Publisher.id == author).all()
+    else:
+        result = my_query.filter(Publisher.name == author).all()
+    for title, name, sale, date in result:
+        print(f'{title:<40} | {name:<8} | {sale:<6} | {date.strftime("%d-%m-%Y")}')
 
+
+login = 'postgres'
+password = '**********'
+db_name = 'netology_db'
+DSN = f'postgresql://{login}:{password}@localhost:5432/{db_name}'
+engine = sq.create_engine(DSN)
+
+if __name__ == '__main__':
+    create_tables(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    insert_data('tests_data.json')
+    author = input('Введите имя или номер автора: ')
+    get_shops(author)
     session.close()
